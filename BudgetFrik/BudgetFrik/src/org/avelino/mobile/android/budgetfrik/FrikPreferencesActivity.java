314b0b8a6bd86ab7876152d49be784eb8eb12ed4
avelino_andretti <avelino_andretti@yahoo.com>
2008-11-22 18:31:57 +0000

    Beta 1

---
+++ BudgetFrik/BudgetFrik/src/org/avelino/mobile/android/budgetfrik/FrikPreferencesActivity.java
@@ -0,0 +1,275 @@
+package org.avelino.mobile.android.budgetfrik;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.avelino.mobile.android.budgetfrik.CategoryDBHelper.Currencies;
+
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.DialogInterface.OnClickListener;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.PreferenceActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.MenuItem.OnMenuItemClickListener;
+/**
+ * License http://creativecommons.org/licenses/by-nc-sa/2.5/se/deed.en_US
+ * See assets/license.html
+ * @author Avelino Benavides
+ *
+ */
+public class FrikPreferencesActivity extends PreferenceActivity {
+
+	public static final String CURRENCIES = "org.avelino.mobile.android.budgetfrik.FrikPreferencesActivity.currency.list";
+	protected static final String AUTOUPDATE = "org.avelino.mobile.android.budgetfrik.FrikPreferencesActivity.auto.update";;
+	private RemoteCurrencyUpdater updater;
+
+	/**
+	 * What I'm I doing wrong with the progress here
+	 * 
+	 * @see android.preference.PreferenceActivity#onCreate(android.os.Bundle)
+	 */
+	@SuppressWarnings("unchecked")
+	protected void onCreate(Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_PROGRESS);
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.frik_prefs);
+		setProgressBarVisibility(false);
+		List<CurrencyTO> curr = (List<CurrencyTO>) getIntent()
+				.getSerializableExtra(CURRENCIES);
+		setCurrencyCombo(curr);
+		EditTextPreference csvPref = (EditTextPreference) getPreferenceScreen()
+				.findPreference(getString(R.string.pref_csv_key));
+		csvPref.setDefaultValue(PreferenceManager.getDefaultCSVSeparator());
+		
+		if (getIntent().getSerializableExtra(AUTOUPDATE)!= null){
+			final AlertDialog updating = new AlertDialog.Builder(FrikPreferencesActivity.this)
+			.setMessage("Updating").create();
+			new OnCurrencyUpdateListener(new ProgressListenerHandler(new Handler()){
+
+				@Override
+				public void setProgress(int p) {
+					
+					super.setProgress(p);
+					if (p >=100){
+						updating.dismiss();
+						show(new AlertDialog.Builder(FrikPreferencesActivity.this)
+						.setMessage("Press the back button to go the main screen"));
+					}
+				}
+				
+			}).onMenuItemClick(null);
+			updating.show();
+			
+		}
+	}
+
+	private void setCurrencyCombo(List<CurrencyTO> curr) {
+		ListPreference curLst = (ListPreference) getPreferenceScreen()
+				.findPreference(getString(R.string.pref_currency_key));
+		curLst.setEntries(CurrencyTO.toStringArray(curr));
+		curLst.setEntryValues(CurrencyTO.toStringValueArray(curr));
+		curLst.setValueIndex(curr.indexOf(new CurrencyTO("", PreferenceManager
+				.getDefaultCurrency(), 0.0f, 0, "")));
+	}
+
+	// protected void onResume() {
+	// super.onResume();
+	//
+	// // Set up a listener whenever a key changes
+	// getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+	// }
+	//
+	// @Override
+	// protected void onPause() {
+	// super.onPause();
+	//
+	// // Unregister the listener whenever a key changes
+	// getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+	//	        
+	// }
+
+	private final class OnCurrencyUpdateListener implements
+			OnMenuItemClickListener {
+		
+		private ProgressListenerHandler progressHandler;
+		public OnCurrencyUpdateListener(){
+		}
+		public OnCurrencyUpdateListener(ProgressListenerHandler progressHandler) {
+			this.progressHandler = progressHandler;
+			
+		}
+
+		public boolean onMenuItemClick(MenuItem arg0) {
+							// dialog.show();
+			
+			new Thread() {
+
+				final Handler mHandler = new Handler();
+
+				@Override
+				public void run() {
+					final CategoryDBHelper dbhelper = new CategoryDBHelper(
+							FrikPreferencesActivity.this);
+					updater = new RemoteCurrencyUpdater(dbhelper);
+					try {
+						setProgressBarVisibility(true);
+						if (progressHandler == null){
+							progressHandler = new ProgressListenerHandler(mHandler);
+						}
+						if (updater
+								.updateCurrencies(progressHandler)) {
+							Cursor c = dbhelper.getAllCurrencies(dbhelper.getReadableDatabase());
+							List<CurrencyTO> l = new ArrayList<CurrencyTO>();
+							c.moveToFirst();
+							while (!c.isAfterLast()) {
+								l.add(new CurrencyTO(c
+										.getString(Currencies.SYMBOL.dbIndex()), 
+										c.getInt(Currencies._ID.dbIndex()),
+										c.getFloat(Currencies.EXCHANGE.dbIndex()), 
+										c.getInt(Currencies.BASE.dbIndex()),
+										c.getString(Currencies.MNEMONIC.dbIndex())));
+								c.moveToNext();
+							}
+							c.close();
+							setCurrencyCombo(l);
+							progressHandler.show(
+									new AlertDialog.Builder(
+										FrikPreferencesActivity.this)
+											.setMessage("Currencies Updated"));
+						} else {
+							progressHandler.show(
+									new AlertDialog.Builder(
+										FrikPreferencesActivity.this)
+											.setMessage(	
+												"Failed to update exhchange rates, try again.\nReason: "
+													+ updater.getErrorReason()));
+						}
+					} finally {
+						setProgressBarVisibility(false);
+
+						dbhelper.close();
+					}
+				}
+
+			}.start();
+
+			return false;
+		}
+	}
+
+	private class ProgressListenerHandler implements ProgressListener{
+		private static final int PROG_RATE = 1000 / 100;
+		private int progress;
+		private final Handler handler; 
+		public ProgressListenerHandler(Handler handler) {
+			this.handler = handler;
+		}
+
+		public void setProgress(int p) {
+			progress = p * PROG_RATE;
+			handler.post(new Runnable(){
+				public void run() {
+					getWindow().setFeatureInt(Window.FEATURE_PROGRESS, progress);
+				}
+
+			});
+		}
+
+
+		public void show(final Builder builder) {
+			handler.post(new Runnable(){
+
+				public void run() {
+					builder.show();
+				}
+				
+			});
+			
+		}
+	}
+
+	public static class PreferenceManager {
+		private PreferenceManager() {
+		}
+
+		private static final String DEFAULT_CURRENCY = "default.currency";
+		private static final String DEFAULT_CSV_SEP = "default.csv.separator";
+		private static final String FIRST_RUN = "first.run";
+
+		public static int getDefaultCurrency() {
+			return Integer.parseInt(prefs.getString(DEFAULT_CURRENCY, "1"));
+		}
+
+		public static String getDefaultCSVSeparator() {
+			return prefs.getString(DEFAULT_CSV_SEP, ",");
+		}
+
+		private static SharedPreferences prefs;
+
+		public static void init(SharedPreferences sharedPreferences) {
+			prefs = sharedPreferences;
+			if (!prefs.contains(DEFAULT_CURRENCY)) {
+				prefs.edit().putString(DEFAULT_CURRENCY, "1").commit();
+			}
+			if (!prefs.contains(DEFAULT_CSV_SEP)) {
+				prefs.edit().putString(DEFAULT_CSV_SEP, ",").commit();
+			}
+		}
+
+		public static boolean firstRun() {
+			return !prefs.contains(FIRST_RUN);
+		}
+
+		public static void unsetFirstRun() {
+			prefs.edit().putString(FIRST_RUN, String.valueOf(System.currentTimeMillis())).commit();
+			
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.prefs_menu, menu);
+		MenuItem item = menu.findItem(R.id.men_pref_clear);
+		item.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+
+			public boolean onMenuItemClick(MenuItem arg0) {
+				final AlertDialog dialog = new AlertDialog.Builder(
+						FrikPreferencesActivity.this).setIcon(
+						android.R.drawable.ic_dialog_alert).setTitle(
+						R.string.txt_pref_confirm).setMessage(
+						R.string.txt_pref_clear_warn).setNegativeButton(
+						android.R.string.cancel, null).setPositiveButton(
+						android.R.string.ok, new OnClickListener() {
+
+							public void onClick(DialogInterface dlg, int which) {
+								new CategoryDBHelper(
+										FrikPreferencesActivity.this)
+										.clearEntries();
+								dlg.dismiss();
+								new AlertDialog.Builder(
+										FrikPreferencesActivity.this)
+										.setMessage("Expenses Cleared").show();
+							}
+						}).create();
+				dialog.show();
+
+				return false;
+			}
+
+		});
+
+		item = menu.findItem(R.id.menu_pref_update);
+		item.setOnMenuItemClickListener(new OnCurrencyUpdateListener());
+		return super.onCreateOptionsMenu(menu);
+	}
+
+}
---
+++ BudgetFrik/BudgetFrik/src/org/avelino/mobile/android/budgetfrik/RemoteCurrencyUpdater.java
@@ -0,0 +1,276 @@
+package org.avelino.mobile.android.budgetfrik;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.avelino.mobile.android.budgetfrik.CategoryDBHelper.Currencies;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+/**
+ * License http://creativecommons.org/licenses/by-nc-sa/2.5/se/deed.en_US
+ * See assets/license.html
+ * @author Avelino Benavides
+ *
+ */
+public class RemoteCurrencyUpdater {
+
+	private static final String TAG = "RemoteCurrencyUpdater";
+	private final CategoryDBHelper dbhelper;
+	private boolean cancelled;
+	private String reason = "N/A";
+
+	public RemoteCurrencyUpdater(CategoryDBHelper dbhelper){
+		this.dbhelper = dbhelper;
+	}
+	
+	public void cancel(){
+		cancelled = true;
+	}
+	
+	public boolean isCanceled(){
+		return cancelled;
+	}
+	
+	public String getErrorReason(){
+		return reason;
+	}
+	
+	//http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml
+	//http://www.ny.frb.org/markets/pilotfx.html
+	public boolean updateCurrencies(ProgressListener listener){
+		cancelled = false;
+//		ConnectivityManager conMan = (ConnectivityManager) activity.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+//		NetworkInfo[] ntwkrs = conMan.getAllNetworkInfo();
+//		for (NetworkInfo info : ntwkrs) {
+//			Log.v(TAG, info.toString());
+//		}
+		listener.setProgress(10);
+//		DefaultHttpClient client = new DefaultHttpClient();
+//		HttpParams params = new BasicHttpParams();
+//		client.setParams(params);
+//		HttpUriRequest request = new HttpGet(ECBCurrencyContentHandler.CURR_URL);
+		
+//		URL url;
+//		HttpURLConnection httpConn = null;
+//		try {
+//			url = new URL("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+//			httpConn = (HttpURLConnection) url.openConnection();
+//			int response = -1;
+//
+//	        httpConn.setAllowUserInteraction(false);
+//	        httpConn.setInstanceFollowRedirects(true);
+//	        httpConn.setRequestMethod("GET");
+//	        httpConn.connect(); 
+//
+//	        response = httpConn.getResponseCode();                 
+//	        if (response != HttpURLConnection.HTTP_OK) {
+//	        	reason = httpConn.getResponseMessage();
+//	            return false;                                 
+//	        }
+//		} catch (MalformedURLException e) {
+//			Log.w(TAG,e);
+//			reason = e.getLocalizedMessage();
+//			return false;
+//		} catch (IOException e) {
+//			Log.w(TAG,e);
+//			reason = e.getLocalizedMessage();
+//			Log.v(TAG,System.getProperties().toString());
+//			return false;
+//		}
+		
+//		System.setProperty("http.proxyHost","alvwebcache1.bastion.europe.hp.com");
+//		System.setProperty("http.proxyPort","8080");
+//		HttpResponse response = null;
+//		try {
+//			response = client.execute(request);
+//			dialog.setProgress(20);
+//			if (response.getStatusLine().getStatusCode() != HttpURLConnection.HTTP_OK){
+//				throw new ClientProtocolException(response.getStatusLine().getReasonPhrase());
+//			}
+//		} catch (ClientProtocolException e) {
+//			Log.w(TAG,e);
+//			reason = e.getLocalizedMessage();
+//			return false;
+//		} catch (IOException e) {
+//			Log.w(TAG,e);
+//			reason = e.getLocalizedMessage();
+//			return false;
+//		}
+		final ECBCurrencyContentHandler handler = new ECBCurrencyContentHandler();
+		if (!cancelled){
+			try {
+				SAXParserFactory
+						.newInstance()
+							.newSAXParser()
+								.parse(ECBCurrencyContentHandler.CURR_URL,
+										handler);//httpConn.getInputStream()));
+				listener.setProgress(40);
+			} catch (SAXException e) {
+				Log.w(TAG,e);
+				reason = e.getLocalizedMessage();
+				return false;
+			} catch (IllegalStateException e) {
+				Log.w(TAG,e);
+				reason = e.getLocalizedMessage();
+				return false;
+			} catch (IOException e) {
+				Log.w(TAG,e);
+				reason = e.getLocalizedMessage();
+				return false;
+			} catch (ParserConfigurationException e) {
+				Log.w(TAG,e);
+				reason = e.getLocalizedMessage();
+				return false;
+			}
+		}
+		if (!cancelled){
+			List<CurrencyTO> targetCurr = new ArrayList<CurrencyTO>(); 
+			Map<String,CurrencyTO> newCurr = handler.getCurrencies();
+			String defCurr = handler.getDefaultMnemonic();
+			CurrencyTO defaultCurr = null;
+			final SQLiteDatabase database = dbhelper.getWritableDatabase();
+			Cursor c = dbhelper.getAllCurrencies(database);
+			listener.setProgress(50);
+			c.moveToFirst();
+			while (!c.isAfterLast()){
+				final String mnemonic = c.getString(Currencies.MNEMONIC.dbIndex());
+				if (defCurr.equals(mnemonic)){
+					defaultCurr = new CurrencyTO(mnemonic, 
+							c.getInt(Currencies._ID.dbIndex()),
+							1.0f,
+							c.getInt(Currencies._ID.dbIndex()),
+						    c.getString(Currencies.MNEMONIC.dbIndex()));
+					targetCurr.add(defaultCurr);
+				} else if (newCurr.containsKey(mnemonic)){
+					targetCurr.add(new CurrencyTO(mnemonic, 
+						c.getInt(Currencies._ID.dbIndex()),
+						newCurr.get(mnemonic).getExchange(),
+						c.getInt(Currencies.BASE.dbIndex()),//Base not set here because we dont know where it is.
+					    c.getString(Currencies.MNEMONIC.dbIndex())));
+					newCurr.remove(mnemonic);
+				}
+				c.moveToNext();
+			}
+			//TODO What if the base currency is not on the list :)
+			for (CurrencyTO curr : targetCurr) {
+				curr.setBase(defaultCurr.getId());
+			}
+			listener.setProgress(60);
+			int prog = 60;
+			float rate = 20/(newCurr.size()+1);
+			for (String mnemonic : newCurr.keySet()) {
+				final CurrencyTO curr = newCurr.get(mnemonic);
+				curr.setSymbol(curr.getMnemonic());
+				curr.setBase(defaultCurr.getId());
+				targetCurr.add(curr);
+				prog+=rate;
+				listener.setProgress(prog);
+			}
+			listener.setProgress(85);
+			dbhelper.inserOrUpdateCurrencies(targetCurr,database);
+			c.close();
+			dbhelper.close();
+		}
+		listener.setProgress(100);
+		return true;
+	}
+	
+
+/**
+ * 
+ * @author benaviav
+ *
+ *<gesmes:Envelope>
+<gesmes:subject>Reference rates</gesmes:subject>
+
+<gesmes:Sender>
+<gesmes:name>European Central Bank</gesmes:name>
+</gesmes:Sender>
+<Cube>
+<Cube time="2008-11-20">
+<Cube currency="USD" rate="1.2542"/>
+<Cube currency="JPY" rate="119.87"/>
+<Cube currency="BGN" rate="1.9558"/>
+<Cube currency="CZK" rate="25.635"/>
+<Cube currency="DKK" rate="7.4524"/>
+<Cube currency="EEK" rate="15.6466"/>
+<Cube currency="GBP" rate="0.84210"/>
+<Cube currency="HUF" rate="269.94"/>
+<Cube currency="LTL" rate="3.4528"/>
+<Cube currency="LVL" rate="0.7093"/>
+<Cube currency="PLN" rate="3.8500"/>
+<Cube currency="RON" rate="3.8170"/>
+<Cube currency="SEK" rate="10.2395"/>
+<Cube currency="SKK" rate="30.380"/>
+<Cube currency="CHF" rate="1.5290"/>
+<Cube currency="ISK" rate="245.00"/>
+<Cube currency="NOK" rate="8.8700"/>
+<Cube currency="HRK" rate="7.1350"/>
+<Cube currency="RUB" rate="34.5555"/>
+<Cube currency="TRY" rate="2.1354"/>
+<Cube currency="AUD" rate="1.9927"/>
+<Cube currency="BRL" rate="3.0198"/>
+<Cube currency="CAD" rate="1.5814"/>
+<Cube currency="CNY" rate="8.5718"/>
+<Cube currency="HKD" rate="9.7202"/>
+<Cube currency="IDR" rate="15363.95"/>
+<Cube currency="KRW" rate="1892.24"/>
+<Cube currency="MXN" rate="16.9003"/>
+<Cube currency="MYR" rate="4.5446"/>
+<Cube currency="NZD" rate="2.3209"/>
+<Cube currency="PHP" rate="62.760"/>
+<Cube currency="SGD" rate="1.9176"/>
+<Cube currency="THB" rate="44.079"/>
+<Cube currency="ZAR" rate="13.2315"/>
+</Cube>
+</Cube>
+</gesmes:Envelope>
+ *
+ */
+	public class ECBCurrencyContentHandler extends DefaultHandler{
+		private static final String EUR = "EUR";
+		private static final String TAG = "ECBCurrencyContentHandler";
+		private static final String CURR_URL = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
+		private Map<String,CurrencyTO> currencies = new HashMap<String,CurrencyTO>();
+		
+		public Map<String,CurrencyTO> getCurrencies(){
+			return currencies;
+		}
+
+		public String getDefaultMnemonic() {
+			return EUR;
+		}
+
+		@Override
+		public void startElement(String uri, String localName, String name,
+				Attributes attributes) throws SAXException {
+			if ("Cube".equals(localName) && attributes.getValue("currency") != null
+					&& attributes.getValue("rate") != null){
+				try {
+					currencies.put(attributes.getValue("currency"),
+							new CurrencyTO("",-1, 
+									DecimalFormat.getNumberInstance().parse(
+											attributes.getValue("rate")).floatValue(),
+									-1,attributes.getValue("currency")));
+				} catch (ParseException e) {
+					Log.w(TAG, "Skipping:" + attributes.getValue("currency") + ", Rate:" + attributes.getValue("rate"),e);
+				}
+			}
+		}
+		
+	}
+	
+}
